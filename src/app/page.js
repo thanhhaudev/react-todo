da178c520161f9209c32ccf8eb14044adb10373b
thanhhaudev <thanhhaudev@gmail.com>
2024-09-20 22:37:01 +0700

    show todos

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Form from "@/components/form";
 import Header from "@/components/header";
@@ -5,12 +6,18 @@ import TODOHero from "@/components/TODOHero";
 import TODOList from "@/components/TODOList";
 
 function Home() {
+    const [todos, setTodos] = React.useState([
+        { title: "Buy groceries", id: self.crypto.randomUUID(), is_completed: false },
+        { title: "Walk the dog", id: self.crypto.randomUUID(), is_completed: false },
+        { title: "Wash the car", id: self.crypto.randomUUID(), is_completed: false },
+    ]);
+
     return (
         <div className="wrapper">
             <Header />
             <TODOHero todos_completed={0} total_todos={0} />
             <Form />
-            <TODOList todos={[]} />
+            <TODOList todos={todos} />
         </div>
     )
 }
