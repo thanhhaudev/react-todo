4a7fccca3aa3962029238154d19f61273e719854
thanhhaudev <thanhhaudev@gmail.com>
2024-09-20 22:54:01 +0700

    show number of todos

--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,16 @@ function Home() {
         { title: "Wash the car", id: self.crypto.randomUUID(), is_completed: false },
     ]);
 
+    const todos_completed = todos.filter(
+        (todo) => todo.is_completed === true
+    ).length;
+
+    const total_todos = todos.length;
+
     return (
         <div className="wrapper">
             <Header />
-            <TODOHero todos_completed={0} total_todos={0} />
+            <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
             <Form />
             <TODOList todos={todos} />
         </div>
