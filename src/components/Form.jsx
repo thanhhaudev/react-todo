70b763230f3936f2ee96c1617e3a38c086059208
thanhhaudev <thanhhaudev@gmail.com>
2024-09-21 21:59:07 +0700

    add required todo name

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,18 @@
 "use client";
+import React from "react";
 import { updateLocalStorageTodos } from "@/utils/localStorageUtils";
 
 function Form({ todos, setTodos }) {
+    const [errMessage, setErrMessage] = React.useState("");
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const value = e.target.todo.value;
+        const value = e.target.todo.value.trim();
+        if (!value) {
+            setErrMessage("Please enter name of the task");
+            return;
+        }
+
         const newTodo = { title: value, id: window.crypto.randomUUID(), is_completed: false };
 
         // update the state with the new todo
@@ -15,25 +22,29 @@ function Form({ todos, setTodos }) {
         updateLocalStorageTodos([...todos, newTodo]);
 
         e.target.reset(); // clear the input field
+        setErrMessage(""); // clear the error message
     };
 
     return (
-        <form className="form" onSubmit={handleSubmit}>
-            <label htmlFor="todo">
-                <input
-                    type="text"
-                    name="todo"
-                    id="todo"
-                    placeholder="Write your next task"
-                />
-            </label>
-            <button>
-                <span className="visually-hidden">Submit</span>
-                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
-                </svg>
-            </button>
-        </form>
+        <>
+            <form className="form" onSubmit={handleSubmit}>
+                <label htmlFor="todo">
+                    <input
+                        type="text"
+                        name="todo"
+                        id="todo"
+                        placeholder="Write your next task"
+                    />
+                </label>
+                <button>
+                    <span className="visually-hidden">Submit</span>
+                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
+                    </svg>
+                </button>
+            </form>
+            { errMessage && <p className="error-message">{errMessage}</p>}
+        </>
     );
 }
 
