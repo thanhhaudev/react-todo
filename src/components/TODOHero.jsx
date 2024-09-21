47c086a3777e1cd4358fb4ebfe7555fe51d8e003
thanhhaudev <thanhhaudev@gmail.com>
2024-09-21 19:00:35 +0700

    update hero component

--- src/components/TODOHero.jsx
+++ src/components/TODOHero.jsx
@@ -4,7 +4,9 @@ function TODOHero({ todos_completed, total_todos }) {
         <section className="todohero_section">
             <div>
                 <p className="text_large">Task Done</p>
-                <p className="text_small">Keep it up</p>
+                <p className="text_small">
+                    { todos_completed === total_todos ? "Congrats! You've completed all tasks" : "Keep it up!" }
+                </p>
             </div>
             <div>
                 {todos_completed}/{total_todos}
